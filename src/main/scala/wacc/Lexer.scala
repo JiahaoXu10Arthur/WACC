0b6b1076ba33deac93dadd4f2d1abc1cad132001
Hengzhi Li <hengzhi.li21@imperial.ac.uk>
2023-01-26 15:30:31 +0000

    Completes lexer for expressions and modifies expression parsing tests [hl5221, jx1921, ly1021, sw2221]

--- src/main/scala/wacc/Lexer.scala
+++ src/main/scala/wacc/Lexer.scala
@@ -1,9 +1,49 @@
 package wacc
 
-import parsley.Parsley 
 import parsley.token.{Lexer, descriptions}
-import descriptions.{LexicalDesc, SpaceDesc}
+import descriptions.{LexicalDesc, SpaceDesc, SymbolDesc}
+import descriptions.text.{TextDesc, EscapeDesc}
+import parsley.implicits.character.{charLift, stringLift}
+import parsley.token.predicate
 
 object Lexer {
-  val lexer = new Lexer(LexicalDesc.plain)
+  private val keywords = Set("true", "false", "begin", "end", "is", "skip", 
+                             "read", "free", "return", "exit", "print", 
+                             "println", "if", "then", "else", "fi", "while", 
+                             "do", "done", "fst", "snd", "newpair", "call", 
+                             "int", "bool", "char", "string", "pair", "null")
+  private val operators = Set("(", ")", ",", "=", ";", "[", "]", "!", "-", 
+                              "len", "ord", "chr", "*", "/", "%", "+", ">", 
+                              ">=", "<", "<=", "==", "!=", "&&", "||")
+  private val escLiterals = Set('0', 'b', 't', 'n', 'f', 'r', '\"', '\'', '\\')
+
+  private val desc = LexicalDesc.plain.copy(
+    spaceDesc = SpaceDesc.plain.copy(
+      commentStart = "#",
+      commentEnd = "\n",
+      space = predicate.Basic(Character.isWhitespace)
+    ),
+
+    symbolDesc = SymbolDesc.plain.copy(
+      hardKeywords = keywords,
+      hardOperators = operators
+    ),
+
+    textDesc = TextDesc.plain.copy(
+      escapeSequences = EscapeDesc.plain.copy(
+        literals = escLiterals
+      )
+    )
+    
+  )
+  val lexer = new Lexer(desc)
+  
+
+  /* Definition for literal tokens */
+  val num = lexer.lexeme.numeric.unsigned.number32[Int]
+  val bool = lexer.lexeme("true" <|> "false")
+  val char = lexer.lexeme.text.character.ascii
+  val str = lexer.lexeme.text.string.ascii
+  val pairLit = lexer.lexeme{"null"}
+  val ident = lexer.lexeme.names.identifier
 }
--- src/test/scala/wacc/ParserExprTest.scala
+++ src/test/scala/wacc/ParserExprTest.scala
@@ -1,30 +1,40 @@
 package wacc
 
-import org.scalatest._
-import flatspec._
-import matchers._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
 
-class ParserExprTest {
+class ParserExprTest extends AnyFlatSpec {
     
-    "int liter" should "be parsed" in pending
+  "int liter" should "be parsed" in {
+		Parser.exprParse("1") shouldBe true
+	}
     
-    "bool liter" should "be parsed" in pending
+  "bool liter" should "be parsed" in {
+		Parser.exprParse("true") shouldBe true
+		Parser.exprParse("false") shouldBe true
+	}
 
-    "char liter" should "be parsed" in pending
+  "char liter" should "be parsed" in {
+		Parser.exprParse('a') shouldBe true
+	}
 
-    "string liter" should "be parsed" in pending
+  "string liter" should "be parsed" in {
+		Parser.exprParse("abc") shouldBe true
+	}
 
-    "pair liter" should "be parsed" in pending
+  "pair liter" should "be parsed" in {
+		Parser.exprParse("()") shouldBe true
+	}
 
-    "identifier" should "be parsed" in pending
+  "identifier" should "be parsed" in pending
 
-    "array element" should "be parsed" in pending
+  "array element" should "be parsed" in pending
 
-    "unary operator <expr>" should "be parsed" in pending
+  "unary operator <expr>" should "be parsed" in pending
 
-    "<expr> binary operator <expr>" should "be parsed" in pending
+  "<expr> binary operator <expr>" should "be parsed" in pending
 
-    "( <expr> )" should "be parsed" in pending
+  "( <expr> )" should "be parsed" in pending
 
 }
 
