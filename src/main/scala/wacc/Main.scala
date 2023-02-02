f510b19f937d66b17892738c2bc6844be4674124
Lydia Yang <ly1021@ic.ac.uk>
2023-02-02 14:54:20 +0000

    Change main to access file from command line. Ammend exit code for valid and syntax error. [jx1921, ly1021]

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -1,10 +1,15 @@
 package wacc
 
 import parsley.{Success, Failure}
+import java.nio.file.Files
+import java.nio.file.Paths
 
 object Main {
 	def main(args: Array[String]): Unit = {
-		Parser.parse(args.head) match {
+		val filename = args.head
+		val string = new String(Files.readAllBytes(Paths.get(filename)))
+
+		Parser.parse(string) match {
 			case Success(x) => {
 				println(s"${args.head} parse success: ")
 				println(x)
