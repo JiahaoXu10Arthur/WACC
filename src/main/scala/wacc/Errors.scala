68a4616121b99b74420d7355ea5f2a3bfe8b891e
Hengzhi Li <hengzhi.li21@imperial.ac.uk>
2023-02-06 15:01:23 +0000

    Implements toString for specialised error

--- src/main/scala/wacc/Errors.scala
+++ src/main/scala/wacc/Errors.scala
@@ -1,9 +1,6 @@
 package wacc
 
-
-
 object Errors {
-// Structure checking
   case class WACCError(source: Option[String], pos: (Int, Int), lines: WACCErrorLines) {
     override def toString(): String = {
       val sourceMsg = source match {
@@ -28,18 +25,21 @@ object Errors {
         case None => ""
         case Some(value) => "mismatched input \'" ++ value.toString ++ "\'"
       }
-
       val expecteds_ = {
-        if (expecteds.isEmpty)
-            ""
-        else 
-            "Expected { " ++ expecteds.map(_.toString()).mkString(", ") ++ " }" }
-
+        if (expecteds.isEmpty) ""
+        else "Expected " ++ expecteds.map(_.toString()).mkString(", ")
+      }
       val reasons_ = reasons.mkString("\n")
-    
       val lineInfo_ = lineInfoFormat(lineInfo).mkString("\n")
-
       unexpected_ ++ "\n" ++ expecteds_ ++ "\n" ++ reasons_ ++ lineInfo_
+    }
+  }
+
+  case class SpecialisedError(msgs: Seq[String], lineInfo: ErrorLineInfo) extends WACCErrorLines {
+    override def toString(): String = {
+      val msgs_ = msgs.mkString("\n")
+      val lineInfo_ = lineInfoFormat(lineInfo).mkString("\n")
+      msgs_ ++ lineInfo_
     }
   }
 
@@ -47,11 +47,8 @@ object Errors {
     Seq(s"$errorLineStart${lineInfo.line}", 
         s"${" " * errorLineStart.length}${errorPointer(lineInfo.errorPointsAt, lineInfo.errorWidth)}")
   }
-
   private val errorLineStart = ">"
   private def errorPointer(caretAt: Int, caretWidth: Int) = s"${" " * caretAt}${"^" * caretWidth}"
-
-  case class SpecialisedError(msgs: Seq[String], lineInfo: ErrorLineInfo) extends WACCErrorLines
   
   sealed trait WACCErrorItem
   case class WACCRaw(item: String) extends WACCErrorItem {
