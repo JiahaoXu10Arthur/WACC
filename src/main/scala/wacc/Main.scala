c998bece8dab524826ef623522f1552679520a19
Lydia Yang <ly1021@ic.ac.uk>
2023-02-10 11:45:09 +0000

    Add comments for symbolTable, Main, semanticChecker [ly1021, sw2221]

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -6,27 +6,36 @@ import java.nio.file.Paths
 import Errors.errorsMkString
 
 object Main {
+  val SuccessCompile = 0
+  val SyntaxError = 100
+  val SemanticError = 200
+
   def main(args: Array[String]): Unit = {
     val filename = args.head
     val string = new String(Files.readAllBytes(Paths.get(filename)))
 
+    /* Syntax Check */
     Parser.parse(string) match {
+      /* Syntax check success */
       case Success(x) => {
-        // println(x)
+        /* Semantic Check */
         val (errors, st) = SemanticChecker.semanticCheck(x)
+        /* No error reported, compile success */
         if (errors.isEmpty) {
           println(s"${args.head} parse success")
-          //	System.exit(0)
+          	System.exit(SuccessCompile)
         } else {
+          /* Error detected, semantic error */
           println(s"${args.head} parse fail: ")
           println(errorsMkString(errors, filename))
-          //	System.exit(200)
+          	System.exit(SemanticError)
         }
       }
+      /* Syntax check failed, syntax error */
       case Failure(err) => {
         println(s"${args.head} parse fail: ")
         println(errorsMkString(Seq(err), filename))
-        //	System.exit(100)
+        	System.exit(SyntaxError)
       }
     }
 
--- src/main/scala/wacc/SemanticChecker.scala
+++ src/main/scala/wacc/SemanticChecker.scala
@@ -11,9 +11,12 @@ object SemanticChecker {
   def semanticCheck(p: Program): (Seq[WACCError], ImmutableSymbolTable) = {
     implicit val st: SymbolTable = new SymbolTable(null)
     implicit val semErr = new ListBuffer[WACCError]()
-
+    
+    // Firstly reading the headeres of the functions
     p.funcs.foreach { f => readInFunctionHeader(f) }
+    // Checking the validity of function declaration
     p.funcs.foreach { f => checkFuncDeclare(f) }
+    // Checking the validity of the statements followed
     p.stats.foreach { s => checkStat(s) }
 
     (semErr.toList, st.getImmutableTable())
